4c4a34d02d6823e03498b23a6bde9b142cf38add
Leilanigreer <leilani@lpcgolf.com>
2024-10-29 17:01:19 -0700

    index action for product

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,9 +1,36 @@
-<script></script>
+<script>
+import axios from "axios";
+import ProductsIndex from "./ProductsIndex.vue";
 
-<template>
-  <main>
-    <h1>Welcome to Vue!</h1>
-  </main>
-</template>
+export default {
+  components: {
+    ProductsIndex,
+  },
+  data: function () {
+   return {
+     products: [],
+   };
+  },
+  created: function () {
+    this.handleIndexProducts();
+  },
+  methods: {
+    handleIndexProducts: function () {
+      axios.get("http://localhost:3000/products.json").then((response) => {
+        console.log("products index", response); 
+        this.products = response.data;
+      });
+    },
+  },
+};
 
-<style></style>
+ </script>
+
+ <template>
+   <main>
+    <ProductsIndex v-bind:products="products"/>
+   </main>
+ </template>
+
+ <style></style>
+
---
+++ src/components/ProductsIndex.vue
@@ -0,0 +1,21 @@
+<script>
+ export default {
+   props: {
+     products: Array,
+   },
+ };
+  </script>
+
+  <template>
+    <div>
+      <h1>All products</h1>
+     <div v-for="product in products" v-bind:key="product.id">
+       <h2>{{ product.name }}</h2>
+       <p>price: {{ product.price }}</p>
+       <p>description: {{ product.description }}</p>
+       <p>supplier_id: {{ product.supplier_id }}</p>
+     </div>
+    </div>
+  </template>
+
+  <style></style>
